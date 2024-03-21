3ba4a672be23a125f2329fb537d0cf41aca61400
Yune Gim <peeper_o_o@naver.com>
2024-03-21 13:25:18 +0900

    Refactor: 노드 삽입시 반환값을 무시할 수 있는 키워드 추가

--- Swift-Practice/DataStructure/SingleLinkedList/DoublyLinkedList.swift
+++ Swift-Practice/DataStructure/SingleLinkedList/DoublyLinkedList.swift
@@ -7,6 +7,7 @@
 
 class DoublyLinkedList<T:Equatable>{
     var head: DoublyNode<T>?
+    @discardableResult
     func insertAtFirst(_ newNode: DoublyNode<T>)->DoublyNode<T>{
         if self.head == nil{
             self.head = newNode
