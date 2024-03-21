cf80853fa0dd5ac37b297df737d30fd2a1e4c8a2
Yune Gim <peeper_o_o@naver.com>
2024-03-21 13:09:42 +0900

    Add: 더블링크드리스트의 가장 첫번째에 노드를 추가하는 기능 구현

--- Swift-Practice/DataStructure/SingleLinkedList/DoublyLinkedList.swift
+++ Swift-Practice/DataStructure/SingleLinkedList/DoublyLinkedList.swift
@@ -7,5 +7,13 @@
 
 class DoublyLinkedList<T:Equatable>{
     var head: DoublyNode<T>?
-    
+    func insertAtFirst(_ newNode: DoublyNode<T>)->DoublyNode<T>{
+        if self.head == nil{
+            self.head = newNode
+            return newNode
+        }
+        newNode.nextNode = self.head
+        self.head = newNode
+        return newNode
+    }
 }
