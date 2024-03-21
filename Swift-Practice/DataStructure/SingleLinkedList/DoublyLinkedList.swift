ec35cbca8ab1e462bb05636b8fcc0ae6a6fb4f11
Yune Gim <peeper_o_o@naver.com>
2024-03-21 13:45:56 +0900

    Comment: 맨 첫부분에 노드를 추가하는 함수에 설명 주석 추가

--- Swift-Practice/DataStructure/SingleLinkedList/DoublyLinkedList.swift
+++ Swift-Practice/DataStructure/SingleLinkedList/DoublyLinkedList.swift
@@ -7,6 +7,7 @@
 
 class DoublyLinkedList<T:Equatable>{
     var head: DoublyNode<T>?
+    ///리스트의 처음에 노드 추가
     @discardableResult
     func insertAtFirst(_ newNode: DoublyNode<T>)->DoublyNode<T>{
         if self.head == nil{
