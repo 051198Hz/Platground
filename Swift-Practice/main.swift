f54a73f00077b736051d2541256d0e2d394daa0c
Yune Gim <peeper_o_o@naver.com>
2024-03-25 15:38:44 +0900

    Chore: project.pbxproj 외

--- Swift-Practice/main.swift
+++ Swift-Practice/main.swift
@@ -5,7 +5,9 @@
 //  Created by Yune gim on 2024/03/20.
 //
 
-import Foundation
-
-print("Hello, World!")
-
+var myDLL: DoublyLinkedList<Int>? = DoublyLinkedList<Int>()
+for i in 0..<10{
+    myDLL!.appendNode(DoublyNode(data: i))
+}
+myDLL!.printReverse()
+myDLL = nil
