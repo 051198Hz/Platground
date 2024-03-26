6ac04235a6d2fb07ccc7f8604396be7d99b9477b
Yune Gim <peeper_o_o@naver.com>
2024-03-26 14:07:29 +0900

    Fix: 수정 노드의 이전 이후 프로퍼티 변경 시 약한 연결로 인해 메모리 해제가 일어나는 오류

--- Swift-Practice/DataStructure/CircularLinkedList/CircularLinkedList.swift
+++ Swift-Practice/DataStructure/CircularLinkedList/CircularLinkedList.swift
@@ -7,7 +7,7 @@
 ///헤드의 이전 노드가 테일, 테일의 다음 노드가 헤드를 가리키는 링크드 리스트
 class CircularLinkedList<T>{
     var head: DoublyNode<T>?
-    weak var tail: DoublyNode<T>?
+    var tail: DoublyNode<T>?
     ///리스트의 처음에 노드 추가
     func inertAtFirst(_ newNode: DoublyNode<T>){
         if self.head == nil{
