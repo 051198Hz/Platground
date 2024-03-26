03a63ad246183e4fd08931e0da4915a3df1d8b08
Yune Gim <peeper_o_o@naver.com>
2024-03-26 12:36:55 +0900

    Feat: 구현 노드를 리스트의 처음에 추가하는 기능

--- Swift-Practice/DataStructure/CircularLinkedList/CircularLinkedList.swift
+++ Swift-Practice/DataStructure/CircularLinkedList/CircularLinkedList.swift
@@ -7,5 +7,19 @@
 ///헤드의 이전 노드가 테일, 테일의 다음 노드가 헤드를 가리키는 링크드 리스트
 class CircularLinkedList<T>{
     var head: DoublyNode<T>?
-    var tail: DoublyNode<T>?
+    weak var tail: DoublyNode<T>?
+    ///리스트의 처음에 노드 추가
+    func inertAtFirst(_ newNode: DoublyNode<T>){
+        if self.head == nil{
+            self.head = newNode
+            self.tail = newNode
+            head?.prevNode = newNode
+            tail?.nextNode = newNode
+            return
+        }
+        newNode.nextNode = self.head
+        newNode.prevNode = self.tail
+        self.head = newNode
+        self.tail?.nextNode = newNode
+    }
 }
