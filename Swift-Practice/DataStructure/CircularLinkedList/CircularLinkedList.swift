79e865ad94fdce5a7898394a294e811170d03aaf
Yune Gim <peeper_o_o@naver.com>
2024-03-26 18:44:52 +0900

    Fix: 수정 리스트를 메모리에서 해제 시 노드들이 dealloc 되지 않는 오류

--- Swift-Practice/DataStructure/CircularLinkedList/CircularLinkedList.swift
+++ Swift-Practice/DataStructure/CircularLinkedList/CircularLinkedList.swift
@@ -6,34 +6,47 @@
 //
 ///헤드의 이전 노드가 테일, 테일의 다음 노드가 헤드를 가리키는 링크드 리스트
 class CircularLinkedList<T>{
-    var head: DoublyNode<T>?
-    var tail: DoublyNode<T>?
+    fileprivate var head: DoublyNode<T>?
+    fileprivate weak var tail: DoublyNode<T>?
+    deinit {
+        var currentNode = self.head
+        while(currentNode != nil){
+            currentNode?.nextNode = nil
+            currentNode = currentNode?.prevNode
+        }
+    }
     ///리스트의 처음에 노드 추가
     func inertAtFirst(_ newNode: DoublyNode<T>){
         if self.head == nil{
             self.head = newNode
             self.tail = newNode
-            head?.prevNode = newNode
-            tail?.nextNode = newNode
             return
         }
-        newNode.nextNode = self.head
+
+        self.head?.prevNode = newNode
+        self.tail?.nextNode = self.head?.prevNode
         newNode.prevNode = self.tail
+        newNode.nextNode = self.head
         self.head = newNode
-        self.tail?.nextNode = newNode
+
     }
     //리스트의 특정 위치에 노드 삽입
+    @discardableResult
     func insertAt(_ idx: Int, _ newNode: DoublyNode<T>)->DoublyNode<T>?{
         if self.head == nil {return nil}
+        
         var currentNode = self.head
         for _ in 0..<idx{
             if currentNode == nil {return nil}
             currentNode = currentNode?.nextNode
         }
+        
         currentNode?.prevNode?.nextNode = newNode
-        currentNode?.prevNode = newNode
-        newNode.nextNode = currentNode
+        currentNode?.prevNode = currentNode?.prevNode?.nextNode
+
         newNode.prevNode = currentNode?.prevNode
+        newNode.nextNode = currentNode
+        
         return newNode
     }
     ///리스트의 마지막에 노드 추가
@@ -41,15 +54,14 @@ class CircularLinkedList<T>{
         if self.head == nil {
             self.head = newNode
             self.tail = newNode
-            head?.prevNode = newNode
-            tail?.nextNode = newNode
+//            head?.prevNode = newNode
+//            tail?.nextNode = newNode
             return
         }
-        self.head?.prevNode = newNode
-        self.tail?.nextNode = newNode
-        newNode.prevNode = self.tail
-        newNode.nextNode = self.head
-        self.tail = newNode
+        newNode.prevNode = self.tail //prevNode는 weak라 rf 증가 안한다
+        newNode.nextNode = self.tail?.nextNode //rf 1 증가, newNode.nextNode = self.head시 head에 대한 rf가 증가함
+        self.tail!.nextNode = newNode //새 노드의 rf 1 증가
+        self.tail = newNode //tail은 weak라 rf 증가 안한다
     }
     ///리스트의 처음 노드를 삭제
     @discardableResult
