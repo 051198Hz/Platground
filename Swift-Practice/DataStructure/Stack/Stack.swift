7c8d9f24907550da469ce2b457fe6c7a45a95dd2
Yune Gim <peeper_o_o@naver.com>
2024-04-01 16:12:48 +0900

    Feat: 구현 Stack의 pop,push 기능

--- Swift-Practice/DataStructure/Stack/Stack.swift
+++ Swift-Practice/DataStructure/Stack/Stack.swift
@@ -6,16 +6,19 @@
 //
 ///입력받은 자료에 대해 후입선출하는  자료구조
 class Stack<T>{
-    class Node{
-        var data: T?
+    var count: Int{
+        return self.stack.count
     }
-    fileprivate(set) var capacity: Int
-    fileprivate(set) var top: Int
-    fileprivate(set) var nodes: [Node]?
-    init(_ size: Int){
-        self.nodes = []
-        self.nodes?.reserveCapacity(size)
-        self.capacity = size
-        self.top = 0
+    var isEmpty: Bool{
+        return stack.isEmpty
+    }
+    private var stack: [T] = []
+    ///새 노드를 스택 위에 올림
+    func push(_ data: T){
+        self.stack.append(data)
+    }
+    ///스택 최상단 노드의 데이터를 반환 후 노드 삭제
+    func pop()->T?{
+        return self.stack.popLast()
     }
 }
