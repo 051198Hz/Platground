6727220ddc7683e46fec87f754ffae4a509530b3
Yune Gim <peeper_o_o@naver.com>
2024-04-04 16:21:29 +0900

    Refactor: 변경 Stack을 Class에서 Struct

--- Swift-Practice/DataStructure/Stack/Stack.swift
+++ Swift-Practice/DataStructure/Stack/Stack.swift
@@ -5,7 +5,7 @@
 //  Created by Yune gim on 2024/03/27.
 //
 ///입력받은 자료에 대해 후입선출하는  자료구조
-class Stack<T>{
+struct Stack<T>{
     var count: Int{
         return self.stack.count
     }
@@ -14,11 +14,11 @@ class Stack<T>{
     }
     private var stack: [T] = []
     ///새 노드를 스택 위에 올림
-    func push(_ data: T){
+    mutating func push(_ data: T){
         self.stack.append(data)
     }
     ///스택 최상단 노드의 데이터를 반환 후 노드 삭제
-    func pop()->T?{
+    mutating func pop()->T?{
         return self.stack.popLast()
     }
 }
