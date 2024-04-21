eb9015c0c9066c72f441fe75a2ac9bd51944e87d
Yune Gim <peeper_o_o@naver.com>
2024-04-21 17:57:37 +0900

    Feat: 추가 덱

--- Swift-Practice/DataStructure/Queue/Queue.swift
+++ Swift-Practice/DataStructure/Queue/Queue.swift
@@ -87,3 +87,49 @@ struct QueueSingleArray<T>{
         return inbox.popLast()
     }
 }
+
+
+struct Deque<T>{
+//    push_front X: 정수 X를 덱의 앞에 넣는다.
+//    push_back X: 정수 X를 덱의 뒤에 넣는다.
+//    pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//    pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//    size: 덱에 들어있는 정수의 개수를 출력한다.
+//    empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+//    front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//    back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+   private var frontBox = QueueSingleArray<T>()
+   private var backBox = QueueSingleArray<T>()
+   init(sequence:[T]){
+       frontBox = QueueSingleArray<T>()
+       backBox = QueueSingleArray(inbox: sequence)
+   }
+   var size: Int{
+       return frontBox.count + backBox.count
+   }
+   var empty: Bool{
+       return frontBox.isEmpty && backBox.isEmpty
+   }
+   var front: T?{
+       if frontBox.isEmpty { return backBox.front }
+       return frontBox.rear
+   }
+   var back: T?{
+       if backBox.isEmpty { return frontBox.front}
+       return backBox.rear
+   }
+   mutating func push_front(_ element: T){
+       frontBox.enqueue(element: element)
+   }
+   mutating func push_back(_ element: T){
+       backBox.enqueue(element: element)
+   }
+   @discardableResult mutating func pop_front()->T?{
+       if frontBox.isEmpty { return backBox.dequeue() }
+       return frontBox.popLast()
+   }
+   @discardableResult mutating func pop_back()->T?{
+       if backBox.isEmpty { return frontBox.dequeue() }
+       return backBox.popLast()
+   }
+}
