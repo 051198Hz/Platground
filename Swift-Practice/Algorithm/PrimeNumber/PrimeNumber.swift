68f0649cb18028863eac1aa2859bac32f4493ece
Yune Gim <peeper_o_o@naver.com>
2024-04-21 18:00:13 +0900

    Doc: 수정 인덴트

--- Swift-Practice/Algorithm/PrimeNumber/PrimeNumber.swift
+++ Swift-Practice/Algorithm/PrimeNumber/PrimeNumber.swift
@@ -13,13 +13,14 @@ struct PrimeNumber{
 
 extension PrimeNumber{
     static func isPrime(number: Int)->Bool{
-       if number == 1 { return false }
-       if number == 2 { return true }
-       if number == 3 { return true }
-       let r = Int(Double(number).squareRoot())
-       for i in 2...r{
-           if number % i == 0 {return false}
-       }
-       return true
-   }
+        if number == 1 { return false }
+        if number == 2 { return true }
+        if number == 3 { return true }
+        let r = Int(Double(number).squareRoot())
+        for i in 2...r{
+            if number % i == 0 {return false}
+        }
+        return true
+    }
+
 }
