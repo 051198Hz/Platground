bee715fe4b0b808d2da4ca27f70074c994632998
Yune Gim <peeper_o_o@naver.com>
2024-04-21 17:56:28 +0900

    Refactor: 수정 배열로 큐를 생성할 수 있음

--- Swift-Practice/DataStructure/Queue/Queue.swift
+++ Swift-Practice/DataStructure/Queue/Queue.swift
@@ -10,6 +10,13 @@ import Foundation
 struct Queue<T>{
     fileprivate var inbox: [T] = []
     fileprivate var outbox: [T] = []
+    init(inbox: [T] = [], outbox: [T] = []) {
+        self.inbox = inbox
+        self.outbox = outbox
+    }
+    var isEmpty: Bool{
+        return outbox.isEmpty && inbox.isEmpty
+    }
     var front: T?{
         return outbox.isEmpty ? inbox.first : outbox.last
     }
@@ -25,11 +32,21 @@ struct Queue<T>{
     }
     mutating func dequeue()->T?{
         if outbox.isEmpty{
+            //outbox.isEmpty == true 일때만 O(n)인 reverse()를 사용하기때문에 전체적으로는 O(1)이라고 할 수 있음
             outbox = inbox.reversed()
             //Array.removeAll()은 keepingCapacity가 false일 때, O(1)의 Performance를 갖는다.
             //https://stackoverflow.com/questions/54133045/performance-array-removeall-vs
             inbox.removeAll()
         }
         return outbox.popLast()
+    }
+    mutating func popLast()->T?{
+        if isEmpty {return nil}
+        if outbox.isEmpty{
+            let ret = outbox.first
+            outbox.removeFirst()
+            return ret
+        }
+        return inbox.popLast()
     }
 }
